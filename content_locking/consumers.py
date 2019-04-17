7f01a6122c82ef8b51f3caa45acc18e0e8ad781a
Michael Hearn <git@mikehearn.net>
2019-04-17 16:50:24 -0400

    Update to support warnings, force unlock & more

--- content_locking/consumers.py
+++ content_locking/consumers.py
@@ -11,6 +11,7 @@ from django.utils.text import slugify
 # at index [0] will be the first user in the story. Users are added on
 # connect() and removed on disconnect().
 
+
 class PresenceConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -19,14 +20,14 @@ class PresenceConsumer(AsyncWebsocketConsumer):
         self.username = None
 
     async def connect(self):
+        """
+        Executes when a user connects to the channel (e.g. entering the page)
+        """
         self.room_name = slugify(self.scope["url_route"]["kwargs"]["room_name"])
         self.room_group_name = "presence_{}".format(self.room_name)
         self.username = self.scope["user"].username
 
-        users_present = cache.get(self.room_name, [])
-        if self.username not in users_present:
-            users_present += [self.username]
-            cache.set(self.room_name, users_present, 7200)
+        self.add_user_to_lock_list(self.username)
 
         await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
@@ -34,19 +35,20 @@ class PresenceConsumer(AsyncWebsocketConsumer):
 
         await self.channel_layer.group_send(
             self.room_group_name,
-            {"type": "send_users", "users": users_present},
+            {"type": "send_users", "users": self.get_users_from_lock_list()},
         )
 
         await self.accept()
 
     async def disconnect(self, code):
-        users_present = cache.get(self.room_name, [])
-        users_present.remove(self.username)
-        cache.set(self.room_name, users_present, 7200)
+        """
+        Executes when a user disconnects from a channel (e.g. leaving the page)
+        """
+        self.remove_user_from_lock_list(self.username)
 
         await self.channel_layer.group_send(
             self.room_group_name,
-            {"type": "send_users", "users": users_present},
+            {"type": "send_users", "users": self.get_users_from_lock_list()},
         )
 
         await self.channel_layer.group_discard(
@@ -54,16 +56,106 @@ class PresenceConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data=None, bytes_data=None):
-        pass
-
-    async def presence(self, event):
-        message = event["message"]
-
-        await self.send(text_data=json.dumps({"message": message}))
+        """
+        Executes any time the channel receives a message from the client
+        """
+        data = json.loads(text_data)
+        if data["event"] == "form_dirty_true":
+            await self.update_dirty_status(True)
+        if data["event"] == "form_dirty_false":
+            await self.update_dirty_status(False)
+        if data["event"] == "force_unlock":
+            await self.take_ownership(self.username)
 
     async def send_users(self, event):
+        """
+        Sends the user dict (owner, is_dirty, user list, etc.) to the client
+        """
         await self.send(
             text_data=json.dumps(
                 {"people_here": event["users"], "current_user": self.username}
             )
+        )
+
+    def add_user_to_lock_list(self, user, owner=False):
+        """
+        The 'lock list' is a list of users who currently have a specific
+        page open for editing (determined by an active websocket connection).
+        This list gets appended as each subsequent user opens the page, which
+        means the first user in the list was chronologically the first one to
+        open the page, and so on.
+
+        The 'owner' of the article is the user who is considered to have the
+        'right" to edit the article, typically either by being chronoloically
+        first to open it, or by having kicked out the original owner.
+
+        This method adds the current user to the article's 'lock list', and if
+        there is no 'owner' (i.e. if no other user is currently editing that
+        article) it sets the current user as the owner.
+        """
+        users = cache.get(
+            self.room_name, {"owner": None, "users_list": [], "is_dirty": False}
+        )
+        if not users.get("owner"):
+            users["owner"] = user
+        users["users_list"].append(user)
+        cache.set(self.room_name, users, 7200)
+
+    def remove_user_from_lock_list(self, user):
+        """
+        Removes a user from the 'lock list' by removing the first occurance
+        of the user's name from the list (a user can potentially be on the list
+        multiple times, if they have the article open in multiple tabs).
+
+        Additionally, if that user was the 'owner' of the article, they are
+        removed and the next person in the lock list is assigned ownership.
+
+        If there is no owner, the article is empty, therefore is_dirty is reset
+        to False and the users dict is in its blank state.
+        """
+        users = cache.get(self.room_name)
+
+        if user in users["users_list"]:
+            users["users_list"].remove(user)
+        if users["owner"] == user:
+            users["owner"] = (
+                users["users_list"][0] if users["users_list"] else None
+            )
+        if not users["owner"]:
+            users["is_dirty"] = False
+        cache.set(self.room_name, users, 7200)
+
+    def get_users_from_lock_list(self):
+        """
+        Returns the users map, including the owner, list of users and is_dirty
+        flag
+        """
+        users = cache.get(self.room_name)
+        return users
+
+    async def update_dirty_status(self, dirty_status):
+        """
+        Sets the is_dirty flag, and transmits the new is_dirty flag to all
+        users in that page (this controls the messaging on the alert box, e.g.
+        "unsaved changes will be lost")
+        """
+        users = cache.get(self.room_name)
+        users["is_dirty"] = dirty_status
+        cache.set(self.room_name, users, 7200)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {"type": "send_users", "users": self.get_users_from_lock_list()},
+        )
+
+    async def take_ownership(self, username):
+        """
+        Sets the current user as the owner of a given article, and transmits
+        that message with the new owner to any user in the same page
+        """
+        users = cache.get(self.room_name)
+        users["owner"] = username
+        cache.set(self.room_name, users, 7200)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {"type": "send_users", "users": self.get_users_from_lock_list()},
         )
---
+++ content_locking/templates/wagtailadmin/pages/content_locking.js
@@ -0,0 +1,168 @@
+/* global Vue, window, WebSocket, $ */
+
+window.contentLockingVueInstance = new Vue({
+  el: '#presence_alert',
+  data: {
+    /**
+     * The following STATE map shows all the various states of the content
+     * locking UI. "Owner" describes the first user into an article; in other
+     * words, the article is unlocked and there is no conflict when the Owner
+     * begins editing.
+     *
+     * "Intruder" describes the second user to enter a page, when the Owner
+     * already has it opened. There can be multiple intruders.
+     *
+     * The STATE value is determined either programatically (through the
+     * `currentState` computed value) or overridden manually via the
+     * `manualState` prop.
+     */
+    STATE: {
+      // Owner's state when an intruder enters the page they are editing
+      OWNER_CONFLICT: 'OWNER_CONFLICT',
+
+      // Intruder's state when entering a page that an owner is already editing
+      INTRUDER_CONFLICT: 'INTRUDER_CONFLICT',
+
+      // Intruder's state after indicating they want to unlock the page
+      INTRUDER_CONFLICT_CONFIRM: 'INTRUDER_CONFLICT_CONFIRM',
+
+      // Intruder's state indicating they want to unlock the page AND the owner
+      // has unsaved changes in their version
+      INTRUDER_CONFLICT_CONFIRM_DIRTY: 'INTRUDER_CONFLICT_CONFIRM_DIRTY',
+
+      // Owner's state when the intrudcer has forced them out of their article
+      OWNER_CONFLICT_FORCED_EXIT: 'OWNER_CONFLICT_FORCED_EXIT',
+
+      // Owner has article opened in multiple tabs/windows
+      OWNER_MULTIPLE_TABS: 'OWNER_MULTIPLE_TABS',
+
+      // Owner has been ejected/usurped from the article
+      OWNER_USURPED: 'OWNER_USURPED',
+    },
+    currentUser: null,
+
+    // Serialized page form data; used for checking clean vs. dirty
+    initialFormData: null,
+    isFormDirty: false,
+    isRemoteFormDirty: false,
+    manualState: null,
+    owner: null,
+    socket: null,
+    usersPresent: [],
+  },
+
+  methods: {
+    unlock: function() {
+      if (this.isRemoteFormDirty) {
+        this.manualState = this.STATE.INTRUDER_CONFLICT_CONFIRM_DIRTY;
+        return;
+      }
+      this.manualState = this.STATE.INTRUDER_CONFLICT_CONFIRM;
+    },
+    forceUnlock: function() {
+      this.socket.send(JSON.stringify({event: 'force_unlock'}));
+      this.manualState = null;
+    },
+    cancelUnlock: function() {
+      this.manualState = null;
+    },
+  },
+
+  mounted: function() {
+    var path = window.location.pathname;
+    var protocol = window.location.protocol == 'http:' ? 'ws://' : 'wss://';
+    this.socket = new WebSocket(
+      protocol + window.location.host + '/ws/content_editing' + path,
+    );
+
+    this.socket.onmessage = e => {
+      var data = JSON.parse(e.data);
+      this.usersPresent = [...data.people_here.users_list];
+      this.owner = data.people_here.owner;
+      this.currentUser = data.current_user;
+      this.isRemoteFormDirty = data.people_here.is_dirty;
+    };
+
+    // Setup listener to detect if form values have changed & set isFormDirty
+    var _this = this;
+    _this.initialFormData = $('#page-edit-form').serialize();
+    $('#page-edit-form').on('change keyup paste', ':input', function() {
+      _this.isFormDirty =
+        _this.initialFormData !== $('#page-edit-form').serialize();
+    });
+  },
+
+  computed: {
+    conflicts: function() {
+      var _this = this;
+      return this.usersPresent.filter(function(item, pos, self) {
+        // Removes current user & dedupes usersPresent array
+        return item != _this.currentUser && self.indexOf(item) == pos;
+      });
+    },
+    currentState: function() {
+      if (this.manualState) {
+        return this.manualState;
+      }
+      if (this.hasConflicts && this.isOwner) {
+        return this.STATE.OWNER_CONFLICT;
+      }
+      if (this.isOpenedInMultipleTabs) {
+        return this.STATE.OWNER_MULTIPLE_TABS;
+      }
+      if (this.hasConflicts && !this.isOwner) {
+        return this.STATE.INTRUDER_CONFLICT;
+      }
+      return null;
+    },
+    hasConflicts: function() {
+      return this.conflicts.length > 0;
+    },
+    isOpenedInMultipleTabs: function() {
+      var _this = this;
+      var currentUsers = this.usersPresent.filter(function(x) {
+        return x == _this.currentUser;
+      });
+      return currentUsers.length > 1;
+    },
+    isOwner: function() {
+      return this.owner === this.currentUser;
+    },
+    locked: function() {
+      return this.currentUser !== this.owner;
+    },
+  },
+
+  watch: {
+    /**
+     * Based on who the owner of the article is, and that value's changes, set
+     * both the conflict state and whether the article form should be locked.
+     */
+    owner: function(owner, oldOwner) {
+      this.manualState = null;
+
+      // Kick out old owner
+      if (this.currentUser === oldOwner && this.currentUser !== owner) {
+        this.manualState = this.STATE.OWNER_USURPED;
+      }
+    },
+
+    /** Identifies if the edit body is grey/inaccessible */
+    locked: function(locked) {
+      $('#page-edit-form :input').prop('disabled', locked);
+    },
+
+    /**
+     * If the form becomes dirty (or clean), send a websocket message to all
+     * users on that page to alter their local isDirty value (used for warning
+     * conflicting users before they overwrite changes).
+     *
+     * This code is here, and not in mounted(), to avoid sending the websocket on
+     * every change; it will only be sent when the form goes from clean to
+     * dirty, or vice versa.
+     */
+    isFormDirty: function(dirty) {
+      this.socket.send(JSON.stringify({event: 'form_dirty_' + dirty}));
+    },
+  },
+});
